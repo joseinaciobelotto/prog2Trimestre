7e65adf7a240560fd9c1f52000a710a8b58aba61
unknown <jibelotto@gmail.com>
2024-06-19 10:56:46 -0300

    aula20

---
+++ src/aula20/exemplo4Herança/Motorista.java
@@ -0,0 +1,17 @@
+package aula20.exemplo4Herança;
+
+public class Motorista extends Funcionario
+{
+
+    private String cnh;
+
+    public String getCnh() {
+        return cnh;
+    }
+
+    public void setCnh(String cnh) {
+        this.cnh = cnh;
+    }
+
+
+}
---
+++ src/aula20/exemplo4Herança/Teste.java
@@ -0,0 +1,18 @@
+package aula20.exemplo4Herança;
+
+public class Teste
+{
+    public static void main(String[] args)
+    {
+        Funcionario f1 = new Funcionario();
+        f1.setNome("Jonas");
+        f1.setSobrenome("Silva");
+        f1.setSalario(1500);
+
+        Motorista m1 = new Motorista();
+
+        m1.setCnh("12311");
+    }
+
+
+}
